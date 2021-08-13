e8d62b02e40a4900d7286721185c39cf96ba58b7
Jose-Ilafaya <ilafayajose@gmail.com>
2021-08-13 17:02:49 -0300

    ejercicio 1

---
+++ ejercicio1/index.js
@@ -0,0 +1,15 @@
+const mostrar = () =>{
+
+    const ape = document.querySelector("#inp_apellido").value;
+    const nom = document.querySelector("#inp_nombre").value;
+
+    console.log(`
+        Mi apellido es: ${ape}
+        Mi nombre es: ${nom}
+
+    `)
+}
+
+const boton = document.getElementById("btn_mostrar");
+
+boton.addEventListener("click", mostrar);
